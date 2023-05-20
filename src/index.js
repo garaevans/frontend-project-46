5b93fcbf60c9e25278c7bc69d2da2e06b325962d
Natalia Garaeva <garaevans@gmail.com>
2023-05-20 17:53:19 +0500

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
 import buildTree from './buildTree.js';
-import formatAsStylish from './formatters/stylish.js';
+import format from './formatters/index.js';
 
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const getData = (filepath) => fs.readFileSync(filepath, 'utf-8');
 const getExtention = (filepath) => path.extname(filepath).slice(1);
 
-export default (filepath1, filepath2) => {
+export default (filepath1, filepath2, formatter = 'stylish') => {
   const data1 = getData(getAbsolutePath(filepath1));
   const data2 = getData(getAbsolutePath(filepath2));
 
@@ -17,5 +17,5 @@ export default (filepath1, filepath2) => {
 
   const tree = buildTree(obj1, obj2);
 
-  return formatAsStylish(tree);
+  return format(tree, formatter);
 };
