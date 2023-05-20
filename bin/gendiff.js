75402ccb8c1e0f344e37a6a3dd863b2e5a32e3e5
Natalia Garaeva <garaevans@gmail.com>
2023-05-20 18:15:56 +0500

    Add default output format to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,10 +7,10 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
-  })
-  .option('-f, --format <type>', 'output format');
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
 
 program.parse();
