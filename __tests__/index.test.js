13015257a7c9efd6afe217cc990c177163df15e4
Natalia Garaeva <garaevans@gmail.com>
2023-05-20 13:44:58 +0500

    Fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,27 +8,20 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expected = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const jsonFile1 = getFixturePath('file1.json');
+const jsonFile2 = getFixturePath('file2.json');
 
-describe('compare flat files', () => {
-  test('json', () => {
-    const filepath1 = getFixturePath('file1.json');
-    const filepath2 = getFixturePath('file2.json');
+const yamlFile1 = getFixturePath('file1.yml');
+const yamlFile2 = getFixturePath('file2.yml');
 
-    expect(genDiff(filepath1, filepath2)).toBe(expected);
+const expectedStylish = fs.readFileSync(getFixturePath('expectedStylish.txt'), 'utf-8');
+
+describe('generate difference', () => {
+  test('json files', () => {
+    expect(genDiff(jsonFile1, jsonFile2)).toBe(expectedStylish);
   });
 
-  test('yaml', () => {
-    const filepath1 = getFixturePath('file1.yml');
-    const filepath2 = getFixturePath('file2.yml');
-
-    expect(genDiff(filepath1, filepath2)).toBe(expected);
+  test('yaml files', () => {
+    expect(genDiff(yamlFile1, yamlFile2)).toBe(expectedStylish);
   });
 });
