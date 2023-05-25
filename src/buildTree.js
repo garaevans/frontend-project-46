337ce0f2c6e9dc27c50be3477883d790df86514c
Natalia Garaeva <garaevans@gmail.com>
2023-05-25 14:55:10 +0500

    Fix buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -7,21 +7,24 @@ const buildTree = (obj1, obj2) => {
 
   return allKeys.map((key) => {
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return { key, value: buildTree(obj1[key], obj2[key]), type: 'nested' };
+      return { key, type: 'hasChildren', children: buildTree(obj1[key], obj2[key]) };
     }
-    if (!_.has(obj1, key)) {
-      return { key, value: obj2[key], type: 'added' };
-    }
+
     if (!_.has(obj2, key)) {
-      return { key, value: obj1[key], type: 'removed' };
+      return { key, type: 'removed', value: obj1[key] };
     }
+
+    if (!_.has(obj1, key)) {
+      return { key, type: 'added', value: obj2[key] };
+    }
+
     if (obj1[key] !== obj2[key]) {
       return {
-        key, oldValue: obj1[key], newValue: obj2[key], type: 'changed',
+        key, type: 'updated', oldValue: obj1[key], newValue: obj2[key],
       };
     }
 
-    return { key, value: obj1[key], type: 'unchanged' };
+    return { key, type: 'unchanged', value: obj1[key] };
   });
 };
 
