fbf42d12e0942a68ebc9a5c878384c84ac947fd8
Natalia Garaeva <garaevans@gmail.com>
2023-05-25 21:10:08 +0500

    Fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,23 +14,23 @@ const jsonFile2 = getFixturePath('file2.json');
 const yamlFile1 = getFixturePath('file1.yml');
 const yamlFile2 = getFixturePath('file2.yml');
 
-const expectedStylish = fs.readFileSync(getFixturePath('expectedStylish.txt'), 'utf-8');
-const expectedPlain = fs.readFileSync(getFixturePath('expectedPlain.txt'), 'utf-8');
-const expectedJSON = fs.readFileSync(getFixturePath('expectedJSON.txt'), 'utf-8');
+const stylishOutput = fs.readFileSync(getFixturePath('stylishOutput.txt'), 'utf-8');
+const plainOutput = fs.readFileSync(getFixturePath('plainOutput.txt'), 'utf-8');
+const jsonOutput = fs.readFileSync(getFixturePath('jsonOutput.txt'), 'utf-8');
 
 describe('generate difference', () => {
   test('stylish format', () => {
-    expect(genDiff(jsonFile1, jsonFile2)).toBe(expectedStylish);
-    expect(genDiff(yamlFile1, yamlFile2)).toBe(expectedStylish);
+    expect(genDiff(jsonFile1, jsonFile2)).toBe(stylishOutput);
+    expect(genDiff(yamlFile1, yamlFile2)).toBe(stylishOutput);
   });
 
   test('plain format', () => {
-    expect(genDiff(jsonFile1, jsonFile2, 'plain')).toBe(expectedPlain);
-    expect(genDiff(yamlFile1, yamlFile2, 'plain')).toBe(expectedPlain);
+    expect(genDiff(jsonFile1, jsonFile2, 'plain')).toBe(plainOutput);
+    expect(genDiff(yamlFile1, yamlFile2, 'plain')).toBe(plainOutput);
   });
 
   test('json format', () => {
-    expect(genDiff(jsonFile1, jsonFile2, 'json')).toBe(expectedJSON);
-    expect(genDiff(yamlFile1, yamlFile2, 'json')).toBe(expectedJSON);
+    expect(genDiff(jsonFile1, jsonFile2, 'json')).toBe(jsonOutput);
+    expect(genDiff(yamlFile1, yamlFile2, 'json')).toBe(jsonOutput);
   });
 });
