25ad1957dce48d566453c92260c8e389cd374146
Natalia Garaeva <garaevans@gmail.com>
2023-10-05 03:57:36 +0500

    Fix stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,50 +1,45 @@
 import _ from 'lodash';
 
-const space = ' ';
+const replacer = ' ';
 const spacesCount = 4;
 
+const makeIndent = (depth, leftShift = 0) => replacer.repeat(depth * spacesCount - leftShift);
+
 const stringify = (data, depth) => {
-  if (!_.isObject(data)) {
+  if (!_.isPlainObject(data)) {
     return `${data}`;
   }
 
-  const indent = space.repeat(depth * spacesCount);
-  const bracketIndent = space.repeat((depth - 1) * spacesCount);
-
   const lines = Object
     .entries(data)
-    .map(([key, value]) => `${indent}${key}: ${stringify(value, depth + 1)}`);
+    .map(([key, value]) => `${makeIndent(depth)}${key}: ${stringify(value, depth + 1)}`);
 
   return [
     '{',
     ...lines,
-    `${bracketIndent}}`,
+    `${makeIndent(depth, 4)}}`,
   ].join('\n');
 };
 
 const formatAsStylish = (tree) => {
   const iter = (node, depth) => {
-    const indent = space.repeat(depth * spacesCount);
-    const shortIndent = space.repeat(spacesCount * depth - 2);
-    const bracketIndent = space.repeat((depth - 1) * spacesCount);
-
     const lines = node.map(({
       key, type, value, oldValue, newValue, children,
     }) => {
       switch (type) {
         case 'removed':
-          return `${shortIndent}- ${key}: ${stringify(value, depth + 1)}`;
+          return `${makeIndent(depth, 2)}- ${key}: ${stringify(value, depth + 1)}`;
         case 'added':
-          return `${shortIndent}+ ${key}: ${stringify(value, depth + 1)}`;
+          return `${makeIndent(depth, 2)}+ ${key}: ${stringify(value, depth + 1)}`;
         case 'updated':
           return [
-            `${shortIndent}- ${key}: ${stringify(oldValue, depth + 1)}`,
-            `${shortIndent}+ ${key}: ${stringify(newValue, depth + 1)}`,
+            `${makeIndent(depth, 2)}- ${key}: ${stringify(oldValue, depth + 1)}`,
+            `${makeIndent(depth, 2)}+ ${key}: ${stringify(newValue, depth + 1)}`,
           ].join('\n');
         case 'unchanged':
-          return `${indent}${key}: ${stringify(value, depth + 1)}`;
-        case 'hasChildren':
-          return `${indent}${key}: ${iter(children, depth + 1)}`;
+          return `${makeIndent(depth)}${key}: ${stringify(value, depth + 1)}`;
+        case 'nested':
+          return `${makeIndent(depth)}${key}: ${iter(children, depth + 1)}`;
         default:
           throw new Error(`Unknown node type: ${type}`);
       }
@@ -53,7 +48,7 @@ const formatAsStylish = (tree) => {
     return [
       '{',
       ...lines,
-      `${bracketIndent}}`,
+      `${makeIndent(depth, 4)}}`,
     ].join('\n');
   };
 
