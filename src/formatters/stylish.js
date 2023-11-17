31000bfb49fe07a659c1377af5d7fddc6df54773
Natalia Garaeva <garaevans@gmail.com>
2023-11-17 12:31:45 +0500

    Fix stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -7,7 +7,7 @@ const makeIndent = (depth, leftShift = 0) => replacer.repeat(depth * spacesCount
 
 const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
-    return `${data}`;
+    return String(data);
   }
 
   const lines = Object
@@ -23,25 +23,23 @@ const stringify = (data, depth) => {
 
 const formatAsStylish = (tree) => {
   const iter = (node, depth) => {
-    const lines = node.map(({
-      key, type, value, oldValue, newValue, children,
-    }) => {
-      switch (type) {
+    const lines = node.map((item) => {
+      switch (item.type) {
         case 'removed':
-          return `${makeIndent(depth, 2)}- ${key}: ${stringify(value, depth + 1)}`;
+          return `${makeIndent(depth, 2)}- ${item.key}: ${stringify(item.value, depth + 1)}`;
         case 'added':
-          return `${makeIndent(depth, 2)}+ ${key}: ${stringify(value, depth + 1)}`;
+          return `${makeIndent(depth, 2)}+ ${item.key}: ${stringify(item.value, depth + 1)}`;
         case 'updated':
           return [
-            `${makeIndent(depth, 2)}- ${key}: ${stringify(oldValue, depth + 1)}`,
-            `${makeIndent(depth, 2)}+ ${key}: ${stringify(newValue, depth + 1)}`,
+            `${makeIndent(depth, 2)}- ${item.key}: ${stringify(item.value1, depth + 1)}`,
+            `${makeIndent(depth, 2)}+ ${item.key}: ${stringify(item.value2, depth + 1)}`,
           ].join('\n');
         case 'unchanged':
-          return `${makeIndent(depth)}${key}: ${stringify(value, depth + 1)}`;
+          return `${makeIndent(depth)}${item.key}: ${stringify(item.value, depth + 1)}`;
         case 'nested':
-          return `${makeIndent(depth)}${key}: ${iter(children, depth + 1)}`;
+          return `${makeIndent(depth)}${item.key}: ${iter(item.children, depth + 1)}`;
         default:
-          throw new Error(`Unknown node type: ${type}`);
+          throw new Error(`Unknown node type: ${item.type}`);
       }
     });
 
